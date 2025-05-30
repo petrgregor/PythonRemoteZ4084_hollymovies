a706e63f960210ecd940641870101a3a70afad1b
Petr Gregor <petr.gregor@email.cz>
2025-05-30 18:24:51 +0200

    Registered Movie model to admin.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from viewer.models import Genre, Country, Creator
+from viewer.models import Genre, Country, Creator, Movie
 
 admin.site.register(Country)
 admin.site.register(Creator)
 admin.site.register(Genre)
+admin.site.register(Movie)
