cad1eb4c172d2cd14fba3f321596547f6bc1de38
Petr Gregor <petr.gregor@email.cz>
2025-06-02 19:04:33 +0200

    Templates (hello).

--- viewer/views.py
+++ viewer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello(request):
+    # práce s databází
+    #context = {'adjectives': ['nice', 'cruel', 'blue', 'beautiful']}
+
+    adjectives = ['nice', 'cruel', 'blue', 'beautiful']
+    name = 'Petr'
+    context = {'adjectives': adjectives, 'name': name}
+    return render(request=request,
+                  template_name='hello.html',
+                  context=context)
