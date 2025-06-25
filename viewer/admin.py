0421fdb0dd047004690521a56f18df0c9798ec80
Petr Gregor <petr.gregor@email.cz>
2025-06-25 19:50:46 +0200

    Admin panel - MovieAdmin: ListView.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from viewer.models import Genre, Country, Creator, Movie
+
+
+class MovieAdmin(ModelAdmin):
+
+    # ListView
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description=None)
+
+    list_display = ['id', 'title_orig', 'title_cz', 'year']
+    list_display_links = ['id', 'title_orig', 'title_cz']
+    ordering = ['title_cz']
+    list_per_page = 10
+    list_filter = ['genres', 'countries']
+    search_fields = ['title_orig', 'title_cz']
+    actions = ['cleanup_description']
+
 
 admin.site.register(Country)
 admin.site.register(Creator)
 admin.site.register(Genre)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
